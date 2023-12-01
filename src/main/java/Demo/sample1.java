2712525cde9d4f8b49c80417998c5ea442751116
Rahul Namo <Rahul Namo@LAPTOP-KDQ9TCCH>
2023-12-02 05:25:37 +0530

    [Priyanka]:1st changes in repository.

---
+++ src/main/java/Demo/sample1.java
@@ -0,0 +1,9 @@
+package Demo;
+
+public class sample1 
+{
+public static void main(String[] args)
+{
+	System.out.println("hi..helloo");
+}
+}
---
+++ src/main/java/LibraryFile/Demo1.java
@@ -0,0 +1,9 @@
+package LibraryFile;
+
+public class Demo1
+{
+public static void main(String[] args) {
+	
+	System.out.println("updating data from file");
+}
+}
